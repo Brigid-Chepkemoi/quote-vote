d158a114004f393a9675d8e84a82632ae30125d8
brie <briememo95@gmail.com>
2022-04-11 01:06:48 +0300

    add commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert/alert.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteServiceComponent } from './quote-service/quote-service.component';
+import { QuoteClassComponent } from './quote-class/quote-class.component';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    QuoteServiceComponent,
+    QuoteClassComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quote-service/quote-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-service',
+  templateUrl: './quote-service.component.html',
+  styleUrls: ['./quote-service.component.css']
+})
+export class QuoteServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
