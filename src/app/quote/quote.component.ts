6ce45f1b48f4f4c2a14df7a6a5ede7efd30c89b7
brie <briememo95@gmail.com>
2022-04-11 01:32:48 +0300

    add form details and styling

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '@angular/compiler';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Quotes } from '../quotes';
 
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
-export class QuoteComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
+ newQuote= new Quotes(0,"", "",0, 0, "", new Date());
+ @Output() addQuote = new EventEmitter<Quotes>();
 
+  submitQuote(form:NgForm , data:any){
+
+    if (form.valid) {
+      this.addQuote.emit(data);
+      form.reset();
+    } else {
+      alert("Enter name and quote please");
+    }
+    form.reset()
+// this.addQuote.emit(this.newQuote);
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
